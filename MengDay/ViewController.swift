516792c97aea788bfa01f53fbc69ab653ae01a8c
Weijie Wang <w268wang@uwaterloo.ca>
2016-06-18 09:23:24 -0400

    initialize the project

---
+++ MengDay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MengDay
+//
+//  Created by Weijie Wang on 2016-06-17.
+//  Copyright © 2016 Weijie Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
